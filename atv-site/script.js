78d3027e3fffa49cc03899b11e636b1ec22fd3fe
Madson Radú <madsonraduc@gmail.com>
2025-01-29 21:35:15 -0300

    Add files via upload

---
+++ atv-site/script.js
@@ -0,0 +1,14 @@
+/*function imprestimo(valor) {
+    if(valor <600 ){
+        console.log("impresimo minimo");
+    }else if(valor <10000){
+        console.log("imprestimo medio");   
+    }if(valor <100000){
+        console.log("impretimo maximo");
+    }
+    return`$(valor)`
+}
+console.log(impresntimo("SR.Madson, oque o senhor gostaria?"));*/
+//function=classe 
+let caixa="biscoito"
+alert(caixa);
